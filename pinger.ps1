d579200323ec7d33b3a4bf05fc74c228dc6c4334
Oppslia <148699849+Oppslia@users.noreply.github.com>
2023-11-15 14:26:51 -0500

    Add files via upload

---
+++ pinger.ps1
@@ -0,0 +1,5 @@
+﻿$ipAddress = Read-Host "Enter an Ip"
+
+Start-Transcript -path C:/Script/PingLog.txt -Append
+$pingCommand = 'Ping.exe -t $ipAddress'
+Invoke-Expression $pingCommand | ForEach {"{0} - {1}" -f (Get-Date),$_}
